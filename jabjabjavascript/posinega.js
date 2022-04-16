5f7c22e1d86165e6090cd2c547ce42d223c33ee6
atapetrone <taglepetrone@gmail.com>
2022-04-16 14:01:28 -0500

    Terminado el ejercicio funcion opuesto

---
+++ jabjabjavascript/posinega.js
@@ -0,0 +1,14 @@
+console.log("EMPEZAMOS CON EL EJERCICIO");
+
+function opuesto(elNumerito){
+
+let nuevoNumerito= elNumerito*(-1);
+
+
+
+return nuevoNumerito;
+}
+
+let resultado02= opuesto(55);
+
+console.log(resultado02);
