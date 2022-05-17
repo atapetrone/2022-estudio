842462898f684ec1eebff5efc7c21a354188f260
atapetrone <taglepetrone@gmail.com>
2022-05-16 22:04:45 -0500

    ejercicios jabjab

---
+++ jabjabjavascript/changeCSSwithJS/app.js
@@ -0,0 +1,32 @@
+let bodyEl = document.querySelector("body");
+let divEl = document.querySelector("div");
+let aEl = document.querySelector("a");
+
+
+
+bodyEl.style.backgroundColor= "#f0f0f2";
+bodyEl.style.margin= "0px";
+bodyEl.style.padding= "0px";
+bodyEl.style.fontFamily= "Arial";
+
+divEl.style.width= "600px";
+divEl.style.margin= "5em auto";
+divEl.style.padding= "2em";
+divEl.style.backgroundColor= "#fdfdff";
+divEl.style.borderRadius= "0.5em";
+divEl.style.boxShadow= "2px 3px 7px 2px rgba(0,0,0,0.02";
+
+aEl.style.color="#38488f";
+aEl.style.textDecoration ="none";
+/* 
+a:link, a:visited {
+    color: #38488f;
+    text-decoration: none;
+}
+@media (max-width: 700px) {
+    div {
+        margin: 0 auto;
+        width: auto;
+    }
+}
+     */
--- jabjabjavascript/demoQuerySelector/app.js
+++ jabjabjavascript/demoQuerySelector/app.js
@@ -1,7 +1,14 @@
 
 // aca se selecciona el primer elemento h1 econtrado y la segunda linea modifica el css
+console.log(document.getElementById("logo2"));
+console.log(document.querySelector("h1"));
+
+const elemento001 = document.getElementById("logo2");
+console.log(elemento001);
+
 let elementoH1 = document.querySelector("h1");
 elementoH1.style.color="rgb(120,160,160)";
+elementoH1.style.fontSize="60px"
 
 //aca se crea un elemento p y luego se le mete un valor
 let contenido= document.createElement("p");
@@ -18,6 +25,6 @@ elementoP.style.fontFamily = "fantasy";
 
 //aca selecciono el primer elemento h2 encontrado y modifico su css
 let elementoH2 = document.querySelector("h2");
-elementoH2.style.fontSize ="large"
+elementoH2.style.fontSize ="small"
 
 
